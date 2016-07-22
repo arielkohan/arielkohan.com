cb8c507191b9ee6c9fa38426fb884c5e94decf3b
Ariel <arielk20@gmail.com>
2016-07-22 01:08:26 -0300

    Change in the site's fonts

--- js/init-dialog.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-				var /*dlgtrigger = document.querySelector( '[data-dialog]' ),*/
-					somedialog = document.getElementById( "somedialog" ),
-					dlg = new DialogFx( somedialog );
-
-				/*dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );*/
-
-			})();
